34ca521ddcb68c0a796c28f19714b7443dc710f5
leandro-guimaraes <74996502+sushi-jack@users.noreply.github.com>
2023-05-10 10:27:52 -0300

    Projeto caractere inserido.

---
+++ CaracteresA_Z/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Caracteres
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite 2 caracteres: ");
+        string entrada = Console.ReadLine();
+
+        // Verifica se foram digitados exatamente dois caracteres
+        if (entrada.Length != 2)
+        {
+            Console.WriteLine("Erro: digite exatamente 2 caracteres.");
+            return;
+        }
+
+        char char1 = entrada[0];
+        char char2 = entrada[1];
+
+        // Verifica se os caracteres estão em ordem alfabética
+        if (char1 >= char2)
+        {
+            Console.WriteLine("Erro: os caracteres não estão em ordem alfabética.");
+            return;
+        }
+
+        // Calcula o número de caracteres existentes entre os dois
+        int numChars = char2 - char1 - 1;
+
+        Console.WriteLine("O número de caracteres entre {0} e {1} é: {2}", char1, char2, numChars);
+    }
+}
+
