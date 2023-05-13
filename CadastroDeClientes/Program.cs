4257c6d1126a3685271846563ad89c5fc5e3e23b
leandro-guimaraes <74996502+sushi-jack@users.noreply.github.com>
2023-05-13 16:35:16 -0300

    Inserido desafio Cadastro de Clientes

---
+++ CadastroDeClientes/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Cliente
+{
+    /*
+     Linhas 1 a 8: Inclui as bibliotecas necessárias e define a classe Cliente, que possui as propriedades ID, Nome, Endereco, Telefone e Email.
+     */
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public string Endereco { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
+
+class CadastroCientes
+{
+    static void Main(string[] args)
+    {
+        string arquivo = "clientes.csv";
+        int ultimoID = 0;
+        List<Cliente> clientes = new List<Cliente>();
+
+        // Carrega os dados do arquivo CSV
+        if (File.Exists(arquivo))
+        {
+            using (StreamReader sr = new StreamReader(arquivo))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    string[] campos = linha.Split(',');
+                    Cliente cliente = new Cliente
+                    {
+                        ID = int.Parse(campos[0]),
+                        Nome = campos[1],
+                        Endereco = campos[2],
+                        Telefone = campos[3],
+                        Email = campos[4]
+                    };
+                    clientes.Add(cliente);
+                    ultimoID = cliente.ID;
+                }
+            }
+        }
+
+        // Menu de opções
+        int opcao = 0;
+        while (opcao != 5)
+        {
+            Console.WriteLine("##################################");
+            Console.WriteLine("#   Escolha uma opção:           #");
+            Console.WriteLine("#   1 - Cadastrar                #");
+            Console.WriteLine("#   2 - Editar                   #");
+            Console.WriteLine("#   3 - Excluir                  #");
+            Console.WriteLine("#   4 - Listar todos             #");
+            Console.WriteLine("#   5 - Sair                     #");
+            Console.WriteLine("##################################");
+
+            if (!int.TryParse(Console.ReadLine(), out opcao) || opcao < 1 || opcao > 5)
+            {
+                Console.WriteLine("Opção inválida. Tente novamente.");
+                continue;
+            }
+
+            switch (opcao)
+            {
+                case 1: // Cadastrar
+                    Console.Clear();
+                    Cliente novoCliente = LerDadosCliente(ultimoID + 1);
+                    clientes.Add(novoCliente);
+                    ultimoID++;
+                    SalvarDadosCSV(clientes, arquivo);
+                    Console.WriteLine("Cliente cadastrado com sucesso.");
+                    break;
+
+                case 2: // Editar
+                    Console.Clear();
+                    Console.Write("Informe o ID do cliente a ser editado: ");
+                    if (!int.TryParse(Console.ReadLine(), out int idEditar) || !ClientesContainsID(clientes, idEditar))
+                    {
+                        Console.WriteLine("ID inválido. Tente novamente.");
+                        continue;
+                    }
+                    Cliente clienteEditar = LerDadosCliente(idEditar);
+                    clientes.RemoveAll(c => c.ID == idEditar);
+                    clientes.Add(clienteEditar);
+                    SalvarDadosCSV(clientes, arquivo);
+                    Console.WriteLine("Cliente editado com sucesso.");
+                    break;
+
+                case 3: // Excluir
+                    Console.Clear();
+                    Console.Write("Informe o ID do cliente a ser excluído: ");
+                    if (!int.TryParse(Console.ReadLine(), out int idExcluir) || !ClientesContainsID(clientes, idExcluir))
+                    {
+                        Console.WriteLine("ID inválido. Tente novamente.");
+                        continue;
+                    }
+                    clientes.RemoveAll(c => c.ID == idExcluir);
+                    SalvarDadosCSV(clientes, arquivo);
+                    Console.WriteLine("Cliente excluído com sucesso.");
+                    break;
+
+                case 4: // Listar todos
+                    Console.Clear();
+                    Console.WriteLine("Lista de clientes cadastrados:");
+                    foreach (Cliente cliente in clientes)
+                    {
+                        Console.WriteLine($"ID: {cliente.ID} - Nome: {cliente.Nome} - Endereço: {cliente.Endereco} - Telefone: {cliente.Telefone} - E-mail: {cliente.Email}");
+                    }
+                    break;
+                case 5: // Sair
+                    Console.Clear();
+                    Console.WriteLine("Saindo do programa...");
+                    break;
+            }
+        }
+    }
+
+    // Lê os dados do cliente digitados pelo usuário
+    static Cliente LerDadosCliente(int id)
+    {
+        Cliente cliente = new Cliente { ID = id };
+
+        Console.Write("Nome: ");
+        cliente.Nome = Console.ReadLine();
+
+        Console.Write("Endereço: ");
+        cliente.Endereco = Console.ReadLine();
+
+        Console.Write("Telefone: ");
+        cliente.Telefone = Console.ReadLine();
+
+        while (true) // Laço para validação de e-mail
+        {
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            if (IsValidEmail(email))
+            {
+                cliente.Email = email;
+                break;
+            }
+            Console.WriteLine("E-mail inválido. Tente novamente.");
+        }
+
+        return cliente;
+    }
+
+    // Verifica se o ID está presente na lista de clientes
+    static bool ClientesContainsID(List<Cliente> clientes, int id)
+    {
+        return clientes.Exists(c => c.ID == id);
+    }
+
+    // Salva os dados dos clientes em um arquivo CSV
+    static void SalvarDadosCSV(List<Cliente> clientes, string arquivo)
+    {
+        using (StreamWriter sw = new StreamWriter(arquivo))
+        {
+            foreach (Cliente cliente in clientes)
+            {
+                sw.WriteLine($"{cliente.ID},{cliente.Nome},{cliente.Endereco},{cliente.Telefone},{cliente.Email}");
+            }
+        }
+    }
+
+    // Verifica se o e-mail é válido utilizando expressão regular
+    static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return false;
+        }
+        try
+        {
+            // Utiliza a classe Regex para validar o e-mail
+            return Regex.IsMatch(email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+    }
+}
---
+++ ObjetoMidia/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Midia
+{
+    public int codigo;
+    public string titulo;
+    public int ano;
+    public float valor;
+    public string categoria;
+
+    public Midia(int codigo, string titulo, int ano, float valor, string categoria)
+    {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public void Listar()
+    {
+        Console.WriteLine("Código: {0}", codigo);
+        Console.WriteLine("Título: {0}", titulo);
+        Console.WriteLine("Ano: {0}", ano);
+        Console.WriteLine("Valor: R${0:F2}", valor);
+        Console.WriteLine("Categoria: {0}", categoria);
+    }
+
+    public bool Pesquisar(string termo)
+    {
+        return titulo.ToLower().Contains(termo.ToLower()) ||
+            categoria.ToLower().Contains(termo.ToLower());
+    }
+
+    public void Cadastrar()
+    {
+        Console.Write("Código: ");
+        codigo = int.Parse(Console.ReadLine());
+
+        Console.Write("Título: ");
+        titulo = Console.ReadLine();
+
+        Console.Write("Ano: ");
+        ano = int.Parse(Console.ReadLine());
+
+        Console.Write("Valor: R$");
+        valor = float.Parse(Console.ReadLine());
+
+        Console.Write("Categoria: ");
+        categoria = Console.ReadLine();
+    }
+
+    public void Alterar()
+    {
+        Console.Write("Título: ");
+        titulo = Console.ReadLine();
+
+        Console.Write("Ano: ");
+        ano = int.Parse(Console.ReadLine());
+
+        Console.Write("Valor: R$");
+        valor = float.Parse(Console.ReadLine());
+
+        Console.Write("Categoria: ");
+        categoria = Console.ReadLine();
+    }
+}
+
+class DVD : Midia
+{
+    public string produtor;
+    public string diretor;
+
+    public DVD(int codigo, string titulo, int ano, float valor, string categoria,
+        string produtor, string diretor) : base(codigo, titulo, ano, valor, categoria)
+    {
+        this.produtor = produtor;
+        this.diretor = diretor;
+    }
+
+    public new void Listar()
+    {
+        base.Listar();
+        Console.WriteLine("Produtor: {0}", produtor);
+        Console.WriteLine("Diretor: {0}", diretor);
+    }
+}
+
+class Livro : Midia
+{
+    public string autor;
+    public string isbn;
+
+    public Livro(int codigo, string titulo, int ano, float valor, string categoria,
+        string autor, string isbn) : base(codigo, titulo, ano, valor, categoria)
+    {
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+
+    public new void Listar()
+    {
+        base.Listar();
+        Console.WriteLine("Autor: {0}", autor);
+        Console.WriteLine("ISBN: {0}", isbn);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Midia> midias = new List<Midia>();
+
+        while (true)
+        {
+            Console.WriteLine("1 - Listar midias");
+            Console.WriteLine("2 - Pesquisar midias");
+            Console.WriteLine("3 - Cadastrar midia");
+            Console.WriteLine("4 - Alterar midia");
+            Console.WriteLine("5 - Sair");
+
+            Console.Write("Opção: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    foreach (Midia midia in midias)
+                    {
+                        midia.Listar();
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    Console.Write("Termo de pesquisa: ");
+                    string termo = Console.ReadLine();
+
+                    bool encontrou = false;
+                    foreach (Midia midia in midias)
+                    {
+                        if (midia.Pesquisar(termo))
+                        {
+                            midia.Listar();
+                            Console.WriteLine();
+                            encontrou = true;
+                        }
+                    }
+
+                    if (!encontrou)
+                    {
+                        Console.WriteLine("Nenhuma mídia encontrada para o termo '{0}'", termo);
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("1 - DVD");
+                    Console.WriteLine("2 - Livro");
+
+                    Console.Write("Opção: ");
+                    int opcaoMidia = int.Parse(Console.ReadLine());
+
+                    Console.Write("Código: ");
+                    int codigo = int.Parse(Console.ReadLine());
+
+                    Console.Write("Título: ");
+                    string titulo = Console.ReadLine();
+
+                    Console.Write("Ano: ");
+                    int ano = int.Parse(Console.ReadLine());
+
+                    Console.Write("Valor: R$");
+                    float valor = float.Parse(Console.ReadLine());
+
+                    Console.Write("Categoria: ");
+                    string categoria = Console.ReadLine();
+
+                    switch (opcaoMidia)
+                    {
+                        case 1:
+                            Console.Write("Produtor: ");
+                            string produtor = Console.ReadLine();
+
+                            Console.Write("Diretor: ");
+                            string diretor = Console.ReadLine();
+
+                            midias.Add(new DVD(codigo, titulo, ano, valor, categoria, produtor, diretor));
+                            break;
+
+                        case 2:
+                            Console.Write("Autor: ");
+                            string autor = Console.ReadLine();
+
+                            Console.Write("ISBN: ");
+                            string isbn = Console.ReadLine();
+
+                            midias.Add(new Livro(codigo, titulo, ano, valor, categoria, autor, isbn));
+                            break;
+                    }
+                    break;
+
+                case 4:
+                    Console.Write("Código da mídia a ser alterada: ");
+                    int codigoAlterar = int.Parse(Console.ReadLine());
+
+                    bool encontrouAlterar = false;
+                    foreach (Midia midia in midias)
+                    {
+                        if (midia.codigo == codigoAlterar)
+                        {
+                            midia.Alterar();
+                            encontrouAlterar = true;
+                            break;
+                        }
+                    }
+
+                    if (!encontrouAlterar)
+                    {
+                        Console.WriteLine("Mídia não encontrada");
+                    }
+                    break;
+
+                case 5:
+                    return;
+            }
+        }
+    }
+}
